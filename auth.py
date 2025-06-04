c3029b11cf7aab49ff0cd0da2ab0eb49de0ab4b2
akkhil-avanos <akkhil.morkonda@avanos.com>
2025-06-04 11:36:17 -0400

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Remove password from session state
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password
+        st.text_input(
+            "Please enter the password", 
+            type="password", 
+            on_change=password_entered, 
+            key="password"
+        )
+        return False
+    
+    return st.session_state["password_correct"]
